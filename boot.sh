266b558cadf75cfa626d545fc8d42eb4f9600aed
Fei <49421653+feiyinn@users.noreply.github.com>
2024-07-05 10:50:39 +0800

    Update boot.sh
    Remove -e from echo

--- boot.sh
+++ boot.sh
@@ -9,7 +9,7 @@ ascii_art='______  _______  ___
 
 echo "$ascii_art"
 echo "=> xstation is for fresh Ubuntu 24.04 Server LTS (Non-GUI) installations only!"
-echo -e "\nBegin installation (or abort with ctrl+c)..."
+echo "\nBegin installation (or abort with ctrl+c)..."
 
 sudo apt-get update >/dev/null
 sudo apt-get install -y git >/dev/null
