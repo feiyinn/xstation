9dbb0cacfc581a4e62b05523958e8b32142cbe1d
feiyinn <feiyin@gmail.com>
2024-07-05 18:27:24 +0800

    Work arround for a tmux strange issue

--- install/app-tmux.sh
+++ install/app-tmux.sh
@@ -1,2 +1,6 @@
 sudo apt install -y tmux
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 cp ~/.local/share/xstation/configs/tmux/.tmux.conf ~/.tmux.conf
+cp ~/.local/share/xstation/configs/tmux/key*.* /usr/share/doc/fzf/examples
+cp ~/.local/share/xstation/configs/tmux/completion.zsh /usr/share/doc/fzf/examples
+cp ~/.local/share/xstation/configs/tmux/fzf.vim /usr/share/doc/fzf/examples
