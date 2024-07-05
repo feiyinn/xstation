d897de4b2f410ac558ec50850c0f1ca158603726
feiyinn <feiyin@gmail.com>
2024-07-05 19:01:25 +0800

    correct fzf

--- install/app-tmux.sh
+++ install/app-tmux.sh
@@ -1,6 +1,7 @@
 sudo apt install -y tmux
+sudo apt install -y fzf
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 cp ~/.local/share/xstation/configs/tmux/.tmux.conf ~/.tmux.conf
-cp ~/.local/share/xstation/configs/tmux/key*.* /usr/share/doc/fzf/examples
-cp ~/.local/share/xstation/configs/tmux/completion.zsh /usr/share/doc/fzf/examples
-cp ~/.local/share/xstation/configs/tmux/fzf.vim /usr/share/doc/fzf/examples
+# cp ~/.local/share/xstation/configs/tmux/key*.* /usr/share/doc/fzf/examples
+# cp ~/.local/share/xstation/configs/tmux/completion.zsh /usr/share/doc/fzf/examples
+# cp ~/.local/share/xstation/configs/tmux/fzf.vim /usr/share/doc/fzf/examples
